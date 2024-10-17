6be6973375a4dac8251419034333406b41ec80e0
Simone Samoggia <simone.samoggia2@studio.unibo.it>
2024-10-17 20:24:31 +0200

    imports

--- app/src/main/java/com/lam/pedro/MainActivity.kt
+++ app/src/main/java/com/lam/pedro/MainActivity.kt
@@ -6,15 +6,9 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import com.lam.pedro.ui.theme.PEDROTheme
 
 class MainActivity : ComponentActivity() {
