3828c60acccf2bc365b93d76b7ab095582b32795
SerAn51 <serranoandrea515@gmail.com>
2024-10-20 20:17:07 +0200

    Git add and App Icon

---
+++ app/src/main/java/com/lam/pedro/presentation/HealthConnectApp.kt
@@ -0,0 +1,136 @@
+/*
+ * Copyright 2022 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.lam.pedro.presentation
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Menu
+import androidx.compose.material3.DrawerValue
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ModalDrawerSheet
+import androidx.compose.material3.ModalNavigationDrawer
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Snackbar
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.rememberDrawerState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.health.connect.client.HealthConnectClient.Companion.SDK_AVAILABLE
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
+import com.example.healthconnectsample.data.HealthConnectManager
+import com.lam.pedro.presentation.navigation.Drawer
+import com.lam.pedro.presentation.navigation.HealthConnectNavigation
+import com.lam.pedro.presentation.navigation.Screen
+import com.lam.pedro.R
+import com.lam.pedro.presentation.theme.HealthConnectTheme
+import kotlinx.coroutines.launch
+
+const val TAG = "Health Connect sample"
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HealthConnectApp(healthConnectManager: HealthConnectManager) {
+    HealthConnectTheme {
+        val drawerState = rememberDrawerState(DrawerValue.Closed)
+        val snackbarHostState = remember { SnackbarHostState() }
+        val navController = rememberNavController()
+        val scope = rememberCoroutineScope()
+        val navBackStackEntry by navController.currentBackStackEntryAsState()
+        val currentRoute = navBackStackEntry?.destination?.route
+        val availability by healthConnectManager.availability
+
+        ModalNavigationDrawer(
+            drawerState = drawerState,
+            drawerContent = {
+                if (availability == SDK_AVAILABLE) {
+                    ModalDrawerSheet {
+                        Drawer(
+                            scope = scope,
+                            drawerState = drawerState,
+                            navController = navController
+                        )
+                    }
+                }
+            }
+        ) {
+            Scaffold(
+                topBar = {
+                    TopAppBar(
+                        title = {
+                            val titleId = when (currentRoute) {
+                                Screen.ExerciseSessions.route -> Screen.ExerciseSessions.titleId
+                                Screen.SleepSessions.route -> Screen.SleepSessions.titleId
+                                Screen.InputReadings.route -> Screen.InputReadings.titleId
+                                Screen.DifferentialChanges.route -> Screen.DifferentialChanges.titleId
+                                else -> R.string.app_name
+                            }
+                            Text(text = stringResource(titleId))
+                        },
+                        navigationIcon = {
+                            IconButton(
+                                onClick = {
+                                    if (availability == SDK_AVAILABLE) {
+                                        scope.launch {
+                                            drawerState.open() // Apri il drawer
+                                        }
+                                    }
+                                }
+                            ) {
+                                Icon(
+                                    imageVector = Icons.Rounded.Menu,
+                                    contentDescription = stringResource(id = R.string.menu)
+                                )
+                            }
+                        },
+                        colors = TopAppBarDefaults.topAppBarColors(
+                            containerColor = MaterialTheme.colorScheme.primary
+                        )
+                    )
+                },
+                snackbarHost = {
+                    SnackbarHost(hostState = snackbarHostState) { data ->
+                        Snackbar(
+                            snackbarData = data,
+                            actionColor = MaterialTheme.colorScheme.secondary
+                        )
+                    }
+                }
+            ) { paddingValues ->
+                Box(modifier = Modifier.padding(paddingValues)) {
+                    HealthConnectNavigation(
+                        healthConnectManager = healthConnectManager,
+                        navController = navController,
+                        snackbarHostState = snackbarHostState
+                    )
+                }
+            }
+        }
+    }
+}
+
