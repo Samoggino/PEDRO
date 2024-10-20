17bd3cf134eaf34ad9868704bb63624aed0d3780
SerAn51 <serranoandrea515@gmail.com>
2024-10-20 19:53:09 +0200

    HealthConnectSample implemented with Material3

--- app/src/main/java/com/lam/pedro/Util.kt
+++ app/src/main/java/com/lam/pedro/Util.kt
@@ -17,38 +17,38 @@ package com.lam.pedro
 
 import androidx.compose.material3.SnackbarDuration
 import androidx.compose.material3.SnackbarHostState
-import com.lam.pedro.model.data.dateTimeWithOffsetOrDefault
+import com.example.healthconnectsample.data.dateTimeWithOffsetOrDefault
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
 import java.time.Instant
 import java.time.ZoneOffset
 import java.time.format.DateTimeFormatter
 import java.time.format.FormatStyle
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.launch
 
 /**
  * Shows details of a given throwable in the snackbar
  */
 fun showExceptionSnackbar(
-  snackbarHostState: SnackbarHostState,
-  scope: CoroutineScope,
-  throwable: Throwable?,
+    snackbarHostState: SnackbarHostState,
+    scope: CoroutineScope,
+    throwable: Throwable?
 ) {
-  scope.launch {
-    snackbarHostState.showSnackbar(
-      message = throwable?.localizedMessage ?: "Unknown exception",
-      duration = SnackbarDuration.Short
-    )
-  }
+    scope.launch {
+        snackbarHostState.showSnackbar(
+            message = throwable?.localizedMessage ?: "Unknown exception",
+            duration = SnackbarDuration.Short
+        )
+    }
 }
 
 fun formatDisplayTimeStartEnd(
-  startTime: Instant,
-  startZoneOffset: ZoneOffset?,
-  endTime: Instant,
-  endZoneOffset: ZoneOffset?,
+    startTime: Instant,
+    startZoneOffset: ZoneOffset?,
+    endTime: Instant,
+    endZoneOffset: ZoneOffset?
 ): String {
-  val timeFormatter = DateTimeFormatter.ofLocalizedTime(FormatStyle.SHORT)
-  val start = timeFormatter.format(dateTimeWithOffsetOrDefault(startTime, startZoneOffset))
-  val end = timeFormatter.format(dateTimeWithOffsetOrDefault(endTime, endZoneOffset))
-  return "$start - $end"
+    val timeFormatter = DateTimeFormatter.ofLocalizedTime(FormatStyle.SHORT)
+    val start = timeFormatter.format(dateTimeWithOffsetOrDefault(startTime, startZoneOffset))
+    val end = timeFormatter.format(dateTimeWithOffsetOrDefault(endTime, endZoneOffset))
+    return "$start - $end"
 }
