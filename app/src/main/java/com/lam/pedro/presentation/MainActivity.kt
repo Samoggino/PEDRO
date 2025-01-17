53864546d3d65594864251ff1210dba7f89e8042
Simone Samoggia <simone.samoggia2@studio.unibo.it>
2025-01-17 19:09:31 +0100

    Fixing onboarding

--- app/src/main/java/com/lam/pedro/presentation/MainActivity.kt
+++ app/src/main/java/com/lam/pedro/presentation/MainActivity.kt
@@ -1,23 +1,17 @@
 package com.lam.pedro.presentation
 
-import android.content.Intent
 import android.os.Build
 import android.os.Bundle
 import android.view.WindowManager
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.annotation.RequiresApi
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.CircularProgressIndicator
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
 import androidx.lifecycle.lifecycleScope
 import com.lam.pedro.data.datasource.SecurePreferencesManager
+import com.lam.pedro.data.datasource.SecurePreferencesManager.isOnboardingCompleted
 import com.lam.pedro.presentation.onboarding.OnboardingScreen
 import com.lam.pedro.presentation.onboarding.OnboardingUtils
 import com.lam.pedro.presentation.theme.PedroTheme
@@ -42,62 +36,45 @@ class MainActivity : ComponentActivity() {
         val context = this
 
         lifecycleScope.launch {
-            // Inizializzazione dei singleton
             initializeSingleton(context)
-
-            // Una volta completato, cambia lo stato per caricare la UI
             isInitialized.value = true
         }
 
         setContent {
             PedroTheme {
-                // if (qualcosa) { TODO
-                ShowOnboardingScreen()
-                // } else {
-                // PedroApp()
-                // }
+                if (isInitialized.value)
+                    if (isOnboardingCompleted()) {
+                        setContent {
+                            PedroApp()
+                        }
+                    } else {
+                        ShowOnboardingScreen()
+                    }
             }
         }
     }
 
-
-    private suspend fun initializeSingleton(context: MainActivity) {
-        // Inizializza il SecurePreferencesManager
-        CoroutineScope(Dispatchers.IO).async {
-            SecurePreferencesManager.initialize(context)
-        }.await()
-    }
 
     @RequiresApi(Build.VERSION_CODES.TIRAMISU)
     @Composable
     private fun ShowOnboardingScreen() {
 
-        if (isInitialized.value) {
-            val scope = rememberCoroutineScope()
-            OnboardingScreen {
-                onboardingUtils.setOnboardingCompleted()
+        val scope = rememberCoroutineScope()
+        OnboardingScreen {
+            onboardingUtils.setOnboardingCompleted()
 
-                scope.launch {
-                    setContent {
-                        PedroApp()
-                    }
+            scope.launch {
+                setContent {
+                    PedroApp()
                 }
             }
-        } else {
-            // Mostra un placeholder (es. loading spinner)
-            LoadingScreen()
         }
     }
 }
 
-
-// Un semplice composable per il placeholder di caricamento
-@Composable
-fun LoadingScreen() {
-    Box(
-        contentAlignment = Alignment.Center,
-        modifier = Modifier.fillMaxSize()
-    ) {
-        CircularProgressIndicator(color = MaterialTheme.colorScheme.primary)
-    }
+private suspend fun initializeSingleton(context: MainActivity) {
+    // Inizializza il SecurePreferencesManager
+    CoroutineScope(Dispatchers.IO).async {
+        SecurePreferencesManager.initialize(context)
+    }.await()
 }
--- app/src/main/java/com/lam/pedro/presentation/PedroApp.kt
+++ app/src/main/java/com/lam/pedro/presentation/PedroApp.kt
@@ -5,7 +5,6 @@ import android.os.Build
 import android.util.Log
 import androidx.annotation.RequiresApi
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
@@ -19,10 +18,7 @@ import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.derivedStateOf
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.navigation.compose.currentBackStackEntryAsState
